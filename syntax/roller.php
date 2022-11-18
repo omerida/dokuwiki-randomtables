0255e1a8c5829936e8b3da71fdc0748b330297ce
Oscar Merida <oscar@smartours.com>
2022-11-17 23:24:07 -0500

    Adds a Functional standalone roller widget

--- syntax/roller.php
+++ syntax/roller.php
@@ -1,11 +1,12 @@
 <?php
+
 /**
  * DokuWiki Plugin randomtable roller (Syntax Component)
  *
  * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
  * @author  Oscar Merida <oscar@oscarm.org>
  */
-class syntax_plugin_randomtables_roller  extends \dokuwiki\Extension\SyntaxPlugin
+class syntax_plugin_randomtables_roller extends \dokuwiki\Extension\SyntaxPlugin
 {
     /** @inheritDoc */
     public function getType(): string
@@ -26,16 +27,29 @@ class syntax_plugin_randomtables_roller  extends \dokuwiki\Extension\SyntaxPlugi
     }
 
     /** @inheritDoc */
-	public function connectTo($mode): void
+    public function connectTo($mode): void
     {
-        $this->Lexer->addSpecialPattern('<ROLL_ON[^>]+>', $mode, 'plugin_randomtables_roller');
+        $this->Lexer->addEntryPattern('\<ROLL_ON\s+', $mode, 'plugin_randomtables_roller');
+    }
+
+    /** @inheritDoc */
+    public function postConnect(): void
+    {
+        $this->Lexer->addExitPattern('>', 'plugin_randomtables_roller');
     }
 
     /** @inheritDoc */
     public function handle($match, $state, $pos, Doku_Handler $handler): array
     {
-		preg_match('/\s([A-Za-z0-9_]+)\>$/', $match, $parts);
-		return [$state, $match, $parts[1]];
+        switch ($state) {
+            case DOKU_LEXER_UNMATCHED:
+                return [$state, $match];
+            case DOKU_LEXER_EXIT:
+                return [$state, ''];
+            case DOKU_LEXER_ENTER:
+            default:
+                return [$state, $match];
+        }
     }
 
     /** @inheritDoc */
@@ -43,13 +57,22 @@ class syntax_plugin_randomtables_roller  extends \dokuwiki\Extension\SyntaxPlugi
     {
         if ($mode !== 'xhtml') {
             return false;
-        }		
+        }
+        [$state, $match] = $data;
 
-		[$state, $match, $id] = $data;
-
-		$renderer->doc .= '<div class="randomtable-well"><button class="randomtable" data-src="'. $id . '" data-target="results-'
-					   . $id . '">Roll</button><div id="results-' . $id .'" class="results"></div></div>' . PHP_EOL;
-
+        switch ($state) {
+            case DOKU_LEXER_ENTER:
+                $renderer->doc .= '<div class="randomtable-well">' . PHP_EOL;
+                break;
+            case DOKU_LEXER_UNMATCHED:
+                $id = $data[1];
+                $renderer->doc .= '<button class="randomtable" data-src="' . $id . '" data-target="results-'
+                    . $id . '">Roll</button><div id="results-' . $id . '" class="results"></div>';
+                break;
+            case DOKU_LEXER_EXIT:
+                $renderer->doc .= '</div>' . PHP_EOL;
+                break;
+        }
         return true;
     }
 }
