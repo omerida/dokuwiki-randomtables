11589dc32bdb975e35e90e103a61f64716885e65
Oscar Merida <oscar@oscarm.org>
2023-07-14 01:23:07 -0400

    Add a plugin to allow users to create a select element to choose which table to roll from

---
+++ syntax/pick.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * DokuWiki Plugin randomtable pick (Syntax Component)
+ *
+ * @license GPL 2 http://www.gnu.org/licenses/gpl-2.0.html
+ * @author  Oscar Merida <oscar@oscarm.org>
+ */
+class syntax_plugin_randomtables_pick extends \dokuwiki\Extension\SyntaxPlugin
+{
+    /** @inheritDoc */
+    public function getType(): string
+    {
+        return 'protected';
+    }
+
+    /** @inheritDoc */
+    public function getPType(): string
+    {
+        return 'block';
+    }
+
+    /** @inheritDoc */
+    public function getSort(): int
+    {
+        return 201;
+    }
+
+    /** @inheritDoc */
+    public function connectTo($mode): void
+    {
+        $this->Lexer->addEntryPattern('\<ROLL_PICK\s+', $mode, 'plugin_randomtables_pick');
+    }
+
+    /** @inheritDoc */
+    public function postConnect(): void
+    {
+        $this->Lexer->addExitPattern('>', 'plugin_randomtables_pick');
+    }
+
+    /** @inheritDoc */
+    public function handle($match, $state, $pos, Doku_Handler $handler): array
+    {
+        switch ($state) {
+            case DOKU_LEXER_UNMATCHED:
+                return [$state, $match];
+            case DOKU_LEXER_EXIT:
+                return [$state, ''];
+            case DOKU_LEXER_ENTER:
+            default:
+                return [$state, $match];
+        }
+    }
+
+    /** @inheritDoc */
+    public function render($mode, Doku_Renderer $renderer, $data): bool
+    {
+        if ($mode !== 'xhtml') {
+            return false;
+        }
+        [$state, $match] = $data;
+
+        switch ($state) {
+            case DOKU_LEXER_ENTER:
+                $renderer->doc .= '<div class="randomtable-well">' . PHP_EOL;
+                break;
+            case DOKU_LEXER_UNMATCHED:
+                $id = md5(serialize($data));
+
+                $renderer->doc .= '<select class="randomtable-pick" name="' . $id. '">';
+                $renderer->doc .= '<option value="">Select Table</option>';
+
+                $tables = preg_split('/[\r\n]+/', $match);
+                $tables = array_filter($tables);
+                foreach ($tables as $table) {
+                    if (str_contains($table, ':')) {
+                        [$ident, $label] = explode(':', $table,2);
+                    } else {
+                        $ident = $label = $table;
+                    }
+
+                    $ident =  $renderer->_xmlEntities(trim($ident));
+                    $label =  $renderer->_xmlEntities(trim($label));
+                    $renderer->doc .= "<option vale=\"{$ident}\">{$label}</option>";
+                }
+                $renderer->doc .= '</select>';
+
+                $renderer->doc .= '<button class="randomtable" data-src="' . $id . '" data-target="results-'
+                    . $id . '">Roll</button><div id="results-' . $id . '" class="results"></div>';
+                break;
+            case DOKU_LEXER_EXIT:
+                $renderer->doc .= '</div>' . PHP_EOL;
+                break;
+        }
+        return true;
+    }
+}
